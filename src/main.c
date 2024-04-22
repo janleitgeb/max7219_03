b8cf31c24d5580f51f0c4859fc152578a016bb95
Marrek Nožka <nozka@spseol.cz>
2024-04-22 12:30:06 +0200

    funguje

--- src/main.c
+++ src/main.c
@@ -5,39 +5,86 @@
 #include "milis.h"
 //#include "delay.h"
 //#include "uart1.h"
+#include "max7219.h"
 
-void init(void)
-{
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+#define DIN_PORT GPIOB
+#define DIN_PIN GPIO_PIN_4
+#define CS_PORT GPIOB
+#define CS_PIN GPIO_PIN_3
+#define CLK_PORT GPIOB
+#define CLK_PIN GPIO_PIN_2
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+void init(void) {
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // taktovani MCU na 16MHz
+
+    GPIO_Init(DIN_PORT, DIN_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(CS_PORT, CS_PIN, GPIO_MODE_OUT_PP_HIGH_SLOW);
+    GPIO_Init(CLK_PORT, CLK_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
 
     init_milis();
-    //init_uart1();
+    // init_uart1();
 }
 
+void display(uint8_t address, uint8_t data) {
+    uint8_t mask;
+    LOW(CS); // začátek přenosu
 
-int main(void)
-{
-  
+    /* pošlu adresu */
+    mask = 128;
+    mask = 1 << 7;
+    mask = 0b10000000;
+    while (mask) {
+        if (address & mask) {
+            HIGH(DIN);
+        } else {
+            LOW(DIN);
+        }
+        HIGH(CLK);
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+    /* pošlu data */
+    mask = 0b10000000;
+    while (mask) {
+        if (data & mask) {
+            HIGH(DIN);
+        } else {
+            LOW(DIN);
+        }
+        HIGH(CLK);
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+
+    HIGH(CS); // konec přenosu
+}
+
+int main(void) {
+
     uint32_t time = 0;
+    int8_t number = 0;
 
     init();
 
+    display(DECODE_MODE, 0b11111111);
+    display(SCAN_LIMIT, 7);
+    display(INTENSITY, 1);
+    display(DISPLAY_TEST, DISPLAY_TEST_OFF);
+    display(SHUTDOWN, SHUTDOWN_ON);
+    display(DIGIT0, 0xF);
+    display(DIGIT1, 0xF);
+    display(DIGIT2, 0xF);
+    display(DIGIT3, 0xF);
+    display(DIGIT4, 0xF);
+    display(DIGIT5, 0xF);
+    display(DIGIT6, 0xF);
+    display(DIGIT7, 0xF);
+
     while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
+        if (milis() - time > 333) {
             time = milis();
-            //printf("%ld\n", time);
+            display(DIGIT0, number++);
         }
-        //delay_ms(333);
     }
 }
 
